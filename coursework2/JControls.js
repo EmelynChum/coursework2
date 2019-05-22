ae0c689c93ab79b8ffc32411da9ed8461e830d16
EvanRemnant <50570207+EvanRemnant@users.noreply.github.com>
2019-05-22 13:42:08 +0800

    Update JControls.js
    Carpe

--- coursework2/JControls.js
+++ coursework2/JControls.js
@@ -408,3 +408,48 @@ JControls.Button = Class.create(JControls.Object, {
         return this;
     }
 });
+
+JControls.PictureBox = Class.create(JControls.Object, {
+    picture:null,//图片数据
+    picAlpha:null,//t透明度
+    picState:null,//显示方式（flex，cut）
+    picPosition:null,//cut方式下，从该坐标开始截取图片数据
+    picSize:null,//cut方式下，截取图片的长宽
+    initialize:function ($super,  argP, argWH, argimage) {
+        $super( argP, argWH);
+        this.setPicture(argimage,{x:0,y:0});
+        this.picAlpha=1.0;
+        this.picState = "cut";
+    },
+    setPicture:function(image,position){//设置该对象图片属性
+        if(image){
+            this.picture=image.data;
+            this.picSize={width:image.cellSize.width,height:image.cellSize.height};
+        }
+        if(position)this.picPosition=position;
+        else this.picPosition={x:0,y:0};
+
+        return this;
+    },
+    showing:function ($super, x, y, w, h){//覆盖父类中showing函数
+        var _context=JMain.JForm.context;
+        if(this.picture){
+            _context.save();
+            _context.globalAlpha=this.alpha*this.picAlpha;
+            if (this.picState == "flex") {
+                _context.drawImage(this.picture, 0, 0, this.picture.width, this.picture.height, x, y, w, h);
+            }else if (this.picState == "cut") {
+                _context.drawImage(this.picture, this.picPosition.x, this.picPosition.y, this.picSize.width,
+                    this.picSize.height, x, y, w, h);
+            }
+            _context.restore();
+        }
+        if (this.selected) {
+            _context.strokeStyle = JColor.red.data;
+            _context.lineWidth = 1;
+            _context.strokeRect(x + _context.lineWidth, y + _context.lineWidth,
+                w - _context.lineWidth*2, h - _context.lineWidth*2);
+        }
+        $super(x, y, w, h);//执行父类中showing函数
+    }
+});
