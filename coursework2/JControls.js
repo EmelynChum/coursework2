8ec55989b1518a63c053655961d5de9038c6e07b
Zora1215 <50580536+Zora1215@users.noreply.github.com>
2019-05-22 13:32:56 +0800

    Update JControls.js

--- coursework2/JControls.js
+++ coursework2/JControls.js
@@ -329,3 +329,50 @@ JControls.Form = Class.create(JControls.Object, {//从父类继承
 
     }
 });
+JControls.Panel = Class.create(JControls.Object, {//从父类继承
+    closeButton:null,//关闭按钮
+    title:null,//显示标题的控件
+    isShowTitle:null,//是否显示标题栏
+    titleHeight:40,//标题栏高度
+    initialize:function ($super,  argP, argWH) {
+        $super( argP, argWH);
+        this.titleHeight=40;
+        this.initTitle();
+        this.hideTitle();
+    },
+    initTitle:function(){
+        this.isShowTitle=true;
+        this.title=new JControls.Label({x:0,y:0}).setSize({width:this.size.width,height:this.titleHeight})
+            .setBGColor(JColor.blue).setFontSize(27).setTextBaseline("middle").setFontType("bold").setTextPos({x:20,y:0});
+        this.closeButton=new JControls.Button({x:this.size.width-60,y:0},{width:60,height:this.titleHeight})
+            .setBGColor(JColor.white);
+        this.closeButton.buttonLabel.setText("关闭").setFontColor(JColor.red).setFontSize(22);
+        this.closeButton.onClick=function(){
+            this.parent.visible=false;
+            this.parent.onCloseButtonClick&&this.parent.onCloseButtonClick();
+            return true;
+        }
+        this.addControlInLast([this.title,this.closeButton]);
+    },
+    hideTitle:function(){
+        if(this.isShowTitle){
+            this.isShowTitle=false;
+            this.title.visible=false;
+            this.closeButton.visible=false;
+            for(var i=0;i<this.controls.length;i++){
+                this.controls[i].relativePosition.y-=this.titleHeight;
+            }
+        }
+    },
+    showTitle:function(title){
+        this.title.setText(title);
+        if(!this.isShowTitle){
+            for(var i=0;i<this.controls.length;i++){
+                this.controls[i].relativePosition.y+=this.titleHeight;
+            }
+            this.isShowTitle=true;
+            this.title.visible=true;
+            this.closeButton.visible=true;
+        }
+    },
+});
